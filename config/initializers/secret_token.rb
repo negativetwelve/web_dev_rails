bb5268709f7b2daafc73c186f59961829951e132
Mark Miyashita <negativetwelve@gmail.com>
2013-03-02 17:30:28 -0800

    base app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebDevApp::Application.config.secret_key_base = 'dc57cbbdce4765fa85e0058a508d8686703151d8e2699b49871db4947e8f16b6a45a31b70baff033333078d9812fbb2b0a56c0d51ea1ce0a0da3dfeafbab40af'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WebDevApp::Application.config.session_store :encrypted_cookie_store, key: '_web_dev_app_session'
